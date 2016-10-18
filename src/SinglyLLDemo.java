f105ab0893aa1d7445ea3018204e53ce7d235cd3
Youmin Zheng <zheng.youmin@gmail.com>
2016-10-18 00:13:14 -0400

    Working part 2, added removeLast method to make the Linked list leaky. Need to work on gettting parameter sent into the constructor

--- src/SinglyLLDemo.java
+++ src/SinglyLLDemo.java
@@ -4,15 +4,20 @@
 public class SinglyLLDemo {
     public static void main(String[] args){
         //create a leaky array stack with given capacity
-        LeakLinkedStack<Integer> leakyIntLLStack = new LeakLinkedStack<Integer>();
+        LeakLinkedStack<Integer> leakyIntLLStack = new LeakLinkedStack<Integer>(10);
 
-        for(int i = 0; i < 12; i++){
+        for(int i = 0; i < 15; i++){
             leakyIntLLStack.push(i+1);
         }
+
+        //prints the top element of the stack and size of the stack before popping
+        System.out.println("Top of stack is " + leakyIntLLStack.top() + " with a size of " + leakyIntLLStack.size());
 
         while(!leakyIntLLStack.isEmpty()){
             System.out.println("[" + leakyIntLLStack.pop() + "]");            //pops top element
         }//end while loop
 
-    }
+        //prints the top element of stack and size of the stack after popping
+        System.out.println("Top of stack is " + leakyIntLLStack.top() + " with a size of " + leakyIntLLStack.size());
+    }//end Main
 }
